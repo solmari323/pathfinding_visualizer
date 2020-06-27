032c8e632fe7f89f0405733e39446ba055016335
Marija Soldatovic <soldatovicmarija00@gmail.com>
2020-06-27 16:25:12 +0100

    Initialize project

---
+++ pv/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Pathfindingvisualizer from './PathfindingVisualizer/PathfindingVisualizer'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <Pathfindingvisualizer></Pathfindingvisualizer>
+    </div>
+  );
+}
+
+export default App;
